1f0b7c82850d70050ada20eedf3d07e770b5216e
KateKrechet <k.krechet@yandex.ru>
2023-12-18 22:04:52 +0300

    send email from k.krechet @yandex.ru

--- djangoProject32_SiteProject/settings.py
+++ djangoProject32_SiteProject/settings.py
@@ -127,4 +127,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'k.krechet@yandex.ru'
+EMAIL_HOST_PASSWORD = 'nsfqcekisrfergjm'
+DEFAULT_FROM_EMAIL = 'k.krechet@yandex.ru'
